e3edc16e83bbd285682902643e3d343f51e36ae4
KarinaDas <karinadas060@gmail.com>
2021-07-08 19:14:08 +0530

    hooks

--- src/components/user.jsx
+++ src/components/user.jsx
@@ -1,68 +1,40 @@
-// const User = (props) => {
-//     return (
-//     <div>
-//         <h1>{props.name}</h1>
-//         <h4>{props.description}</h4>
-//     </div>
-//     );
-// };
+import React, { useState, useEffect } from "react";
 
-// export default User;
+const User = (props) => {
+  const [planet, setPlanet] = useState("earth");
 
-// props => properties/parameters
+  // componentDidMount
+  useEffect(() => {
+    // heavy computation
+    console.log("component mounting");
 
-import React from "react";
+    // componentWillUnMount
+    return console.log("bye bye");
+  }, []);
 
-class User extends React.Component {
-    constructor(props) {
-        super(props);
+  // componentDidUpdate
+  // shouldComponentUpdate
+  useEffect(() => {
+    console.log("Planet changes");
+  }, [planet]);
 
-        this.state = {
-            planet : "earth",
-        };
-        console.log("Hey I'm from Constructor");
-    }
-
-    componentDidMount() {
-        this.setState({ planet: "Jupiter"});
-        this.setState({ planet: "Mars"});
-    }
-
-    shouldComponentUpdate(nextProp, nextState) {
-        console.log("from shouldComponentUpdate")
-        console.log({
-            nextProp,
-            nextState,
-        });
-        return true;
-    }
-
-    getSnapshotBeforeUpdate(prevProp, prevState) {
-        console.log("from getSnapshotBeforeUpdate");
-        console.log({ 
-            prevProp, 
-            prevState,
-        });
-        return true;
-    }
-
-    componentDidUpdate() {
-        console.log(this.state);
-    }
-
-    render() {
-        console.log("Hey I'm from rendered");
-//  view
-//  logic
-// Component Life-Cycle Methods
-        return (
-            <div>
-                <h1> {this.props.name} </h1>
-                <p> {this.props.description} </p>
-                <h4> {this.state.planet} </h4>
-            </div>
-        );
-    }
-}
+  return (
+    <div>
+      <h1> {props.name} </h1>
+      <p> {props.description} </p>
+      <button onClick={() => setPlanet("pluto")}>{planet}</button>
+    </div>
+  );
+};
 
 export default User;
+
+// state
+
+// React 16
+
+// Hooks -> power to you functional components
+
+// ->  Life Cycle methods
+
+// -> Hooks
